859f4c874a5e269902333ab0dca1f7b60ef084a8
Creastv <piotr@roial.pl>
2023-02-23 13:56:43 +0100

    mieszkania

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-    <meta name="theme-color" content="#eaf2f7">
+    <meta name="theme-color" content="#5f031f">
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
